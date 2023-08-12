85c95eaab60385bc395dbdd6c576907b387d1aef
amanNeeraj <87442852+amanNeeraj@users.noreply.github.com>
2023-08-12 23:33:54 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AssessmentComponent } from './assessment/assessment.component';
+import { LibraryComponent } from './library/library.component';
+import { RoundStatusComponent } from './round-status/round-status.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { BodyComponent } from './body/body.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    AssessmentComponent,
+    LibraryComponent,
+    RoundStatusComponent,
+    NavigationComponent,
+    BodyComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
